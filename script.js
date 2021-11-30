f8c24afef723e5711b9f6900f385d3b252be5952
Taziiev Andrii <andrew13369@icloud.com>
2021-11-30 12:56:01 +0200

    сделал усложненное задание

--- script.js
+++ script.js
@@ -1,39 +1,10 @@
-'use strict';
-
-// Задаем недели на русском и английском
-const ruWeek = "Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье";
-const enWeek = "Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday";
-const lang = "ru";
-
-// выводим неделю использовуя IF
-if (lang === "ru") {
-    console.log(ruWeek);
-} else if (lang === "en") {
-    console.log(enWeek);
-} else {
-    console.log("язык не опознан");
+function getArgument(str) {
+    if (typeof str != 'string') {
+        console.log("Передана не строка")
+        return;
+    }
+    if (str.length <= 30) return str;
+    return str.trim().slice(0, 30) + '...';
 }
 
-//Выводим неделю используя switch
-switch (lang) {
-    case "ru":
-        console.log(ruWeek);
-        break;
-    case "en":
-        console.log(enWeek);
-        break;
-    default:
-        console.log("язык не опознан");
-}
-
-// Выводим неделю используя многомерный массив
-const langArray = {
-    ru: ruWeek.split(", "),
-    en: enWeek.split(", ")
-};
-console.log(langArray[lang]);
-
-// Выводим обращение к сотруднику используя тернарные операторы
-let namePerson = "";
-console.log(namePerson === "Артем" ? "директор" :
-    namePerson === "Александр" ? "преподаватель" : "студент");
+console.log(getArgument("       12345hdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdfdf"));
